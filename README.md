778d7f077a60dcb17435b2de653dd203f88337a0
Om Ganesh <dreamcodelife@gmail.com>
2020-09-07 22:52:08 -0400

    LK-06 Longest substring without repeating characters (#7)
    https://leetcode.com/problems/longest-substring-without-repeating-characters
    
    Resolves #6

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ This file maintains the details of each program and the related source/other inf
 The repository currently written in C#  (VS 2019 - .net version 8.0)   
 
 ### **Two Sum**  
-Issue:  https://github.com/om-ganesh/leetcode/issues/1  
+Issue:  https://github.com/om-ganesh/leetcode/issues/2  
 Problem: https://leetcode.com/problems/two-sum/  
 Solution: [TwoSum.cs](consoleproject/TwoSum.cs)  
+
+### **Sum of Singly Linked List of numbers**  
+Issue:  https://github.com/om-ganesh/leetcode/issues/4  
+Problem: https://leetcode.com/problems/add-two-numbers/  
+Solution: [AddSinglyLinkedLists.cs](consoleproject/AddSinglyLinkedLists.cs)  
+
+### **Longest Substring without repeating characters**  
+Issue:  https://github.com/om-ganesh/leetcode/issues/6  
+Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/   
+Solution: [LongestSubstring.cs](LongestSubstring/TwoSum.cs)  
+
---
+++ consoleproject/LongestSubstring.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleproject
+{
+    class LongestSubstring
+    {
+        List<string> data = new List<string>();
+        public LongestSubstring()
+        {
+            data.Add("abcabcbb");
+            data.Add("ababc");
+            data.Add("bbbbb");
+            data.Add("pwwkew");
+            data.Add("abcdbebfagcf");
+            data.Add("");
+            data.Add("au");
+            data.Add(" ");
+        }
+
+        public void Execute()
+        {
+            data.ForEach(d =>
+            {
+                var result = LengthOfLongestSubstring(d);
+                Console.WriteLine($"The longest length of substring in [{d}] is {result}");
+            });
+        }
+
+        private int LengthOfLongestSubstring(string s)
+        {
+            int maxLength = 0;
+            Dictionary<char, int> substring = new Dictionary<char, int>();
+
+            for (int i = 0, j=0; j < s.Length; )
+            {
+                if (substring.ContainsKey(s[j]))
+                {
+                    substring.Remove(s[i]);
+                    i=i+1;
+                }
+                else
+                {
+                    substring.Add(s[j], j);
+                    maxLength = (j - i+1) > maxLength ? (j - i+1) : maxLength;
+                    j++;
+                }
+            }
+            return maxLength;
+        }
+
+        private int LengthOfLongestSubstringLegacy(string s)
+        {
+            int maxLength = 0;
+            for(int i=0; i < s.Length; i++)
+            {
+                HashSet<char> visited = new HashSet<char>();
+                int length = 0;
+                for(int j =i; j < s.Length; j++)
+                {
+                    if (!visited.Contains(s[j]))
+                    {
+                        length++;
+                        maxLength = maxLength < length ? length : maxLength;
+                        if(maxLength == s.Length)
+                        {
+                            return maxLength;
+                        }
+                        visited.Add(s[j]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return maxLength;
+        }
+    }
+}
