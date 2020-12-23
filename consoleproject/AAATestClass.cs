213c6d48984a0ee8607fca249b02746d5858c301
Om <dreamcodelife@gmail.com>
2020-12-22 21:34:03 -0500

    Add an empty test class for easy test app

---
+++ consoleproject/AAATestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleproject
+{
+    class AAATestClass
+    {
+        public void Execute()
+        {
+            int[] data = new int[] {3,3 };
+            var result = FindSolution(data);
+            Console.WriteLine(string.Join(",", result));
+        }
+
+        private int FindSolution(int[] arr)
+        {
+            return arr.Aggregate((x, y) => x + y);
+        }
+    }
+}
