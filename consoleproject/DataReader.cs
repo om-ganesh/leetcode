2585da888b871c57df7072b8110dc992c3120cd1
Om Ganesh <dreamcodelife@gmail.com>
2021-10-14 06:34:55 -0400

    Fix TimeOut issue with LongesteConsecutiveCount problem

---
+++ consoleproject/DataReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleproject
+{
+    static class DataReader
+    {
+        public static int[] ReadCsvData(string fileName)
+        {
+            string lineFromFile = File.ReadAllText(fileName);
+            string[] contents = lineFromFile.Split(',');
+            return Array.ConvertAll(contents, int.Parse);
+        }
+    }
+}
