9c465a3b234a56b1b53af309acd9cb5ed4da8d24
Om Ganesh <dreamcodelife@gmail.com>
2022-04-15 21:32:51 -0400

    Add programs practised on NYC meetup (#44)
    -Add the twosum problem

---
+++ NewYorkCoders/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NewYorkCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello NYCoders !!!");
+
+            new TwoSumProblem().Execute();
+
+        }
+    }
+}
