90f51ad54ec2dd6a196e79784536b363bdb11da9
Om Ganesh <dreamcodelife@gmail.com>
2022-04-15 21:27:18 -0400

    Add old solutions from blind75 questions (#43)

---
+++ BlindMustDo75Leetcode/DuplicateContainsProblem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlindMustDo75Leetcode
+{
+    class DuplicateContainsProblem
+    {
+        public DuplicateContainsProblem()
+        {
+            int[] arr = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            Console.WriteLine(ContainsDuplicate(arr));
+        }
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> hash = new HashSet<int>();
+            foreach (int x in nums)
+            {
+                if (hash.Contains(x))
+                {
+                    return true;
+                }
+                hash.Add(x);
+            }
+            return false;
+        }
+    }
+}
